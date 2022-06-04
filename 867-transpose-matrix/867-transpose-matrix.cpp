1ae4af3acfb70564b11c8f887ad1f46867852047
Omar Hisham <52583910+OmarHisham99@users.noreply.github.com>
2022-06-05 01:40:03 +0200

    Time: 21 ms (32.20%), Space: 11 MB (9.78%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         vector<vector<int>> output ; 
         for(int i =0 ; i< matrix[0].size();i++){
             vector<int> v; 
