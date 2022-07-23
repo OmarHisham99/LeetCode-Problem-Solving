3fd6d995e70abd17705ea130ebaef7dc0061c7e5
Omar Hisham <52583910+OmarHisham99@users.noreply.github.com>
2022-07-23 17:18:17 +0200

    Time: 76 ms (82.25%), Space: 43.9 MB (50.17%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -3,19 +3,11 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-    let dp =[];  
-    
-    for(let i=cost.length-1;i>=0;i--){
-       if(i===cost.length-1){
-           dp[i]= cost[i]+0;
-       }
-        else if(i===cost.length-2){
-            dp[i] = cost[i]+0; 
-        }
-        else{
-            dp[i] = cost[i]+Math.min(dp[i+1],dp[i+2]); 
-        }
+   let dp =[cost[0],cost[1]];  
+    let size = cost.length; 
+    for(let i=2;i<cost.length;i++){
+        dp[i] = cost[i]+Math.min(dp[i-1],dp[i-2]); 
     }
-    return Math.min(dp[0],dp[1]); 
+    return Math.min(dp[size-1],dp[size-2]); 
    
 };
