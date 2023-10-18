2c539057d2cd8dcfda362e419daf4339ed7678b6
Omar Hisham <52583910+OmarHisham99@users.noreply.github.com>
2023-10-18 22:45:01 +0300

    Time: 781 ms (5%), Space: 263.1 MB (5.03%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+   int lengthOfLongestSubstring(string s) {
+       if(s.size()==0 || s.size()==1) return s.size();
+     int left = 0, res = INT_MIN;
+     set<char> seen; 
+     for (int i = 0; i < s.size(); i++) {
+          if (seen.count(s[i])) {
+               int size = seen.size();
+               cout << size << endl;
+               res = max(size, res);
+               seen.clear();
+               cout << seen.size() << endl;
+               i = left; 
+               left++;
+          }
+          seen.insert(s[i]);
+     }
+     int size = seen.size(); 
+     res = max(size,res); 
+     return res!=INT_MIN ? res : 0 ;
+}
+};
