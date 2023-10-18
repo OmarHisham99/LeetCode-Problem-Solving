13f8c67cf286231407b2a43ffdf2a6d5d94d74fe
Omar Hisham <52583910+OmarHisham99@users.noreply.github.com>
2023-10-18 05:05:34 +0300

    Time: 4 ms (26.76%), Space: 6.6 MB (30.27%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        set<int> seen; 
+        while(n!=1 && !seen.count(n)){
+            seen.insert(n);
+            int sum = 0;  
+            while(n > 0){
+                int x = n%10; 
+                sum += pow(x,2); 
+                n/=10; 
+            }
+            n = sum; 
+        }
+        return n==1; 
+    }
+};
